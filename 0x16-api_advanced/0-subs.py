8f7863a075ae7f9412633b0a4b73d7bc2b7d98bb
Benjamin <benjminrichie@gmail.com>
2024-05-08 00:19:45 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,10 +10,12 @@ def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    response = requests.get(url, headers=headers)
     if response.status_code == 200:
         data = response.json()
         subscribers = data['data']['subscribers']
+        print("Existing subreddit")
         return subscribers
     else:
+        print("Non-existing subreddit")
         return 0
